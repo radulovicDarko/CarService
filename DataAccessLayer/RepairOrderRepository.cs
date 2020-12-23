557aa8ee85f19a3cdfbc778ddda8c4016b7b3ce2
DESKTOP-GHD4N2G\nikola <nikola.milakovic1999@gmail.com>
2020-12-24 00:55:15 +0100

    Added RepairOrder CRUD

--- DataAccessLayer/RepairOrderRepository.cs
+++ DataAccessLayer/RepairOrderRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Shared.Interfaces.Repository;
+using Shared.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,49 @@ namespace DataAccessLayer.Models
 {
     public class RepairOrderRepository : IRepairOrderRepository
     {
+        public List<RepairOrder> GetAllRepairOrders()
+        {
+            List<RepairOrder> ListToReturn = new List<RepairOrder>();
+
+            SqlDataReader sqlDataReader = DBConnection.GetData("SELECT * from RepairOrders");
+
+            while (sqlDataReader.Read())
+            {
+                RepairOrder temp = new RepairOrder();
+
+                temp.Id = sqlDataReader.GetInt32(0);
+                temp.DateOfReceipt = sqlDataReader.GetString(1);
+                temp.Description = sqlDataReader.GetString(2);
+                temp.Price = sqlDataReader.GetDecimal(3);
+
+                ListToReturn.Add(temp);
+            }
+
+            return ListToReturn;
+
+        }
+
+        public int InsertRepairOrder(RepairOrder repairOrder)
+        {
+            var result = DBConnection.EditData("INSERT INTO RepairOrders VALUES('(0)', '(1)', '(2)', '(3)");
+
+            return result;
+        }
+
+        public int UpdateRepairOrder(RepairOrder repairOrder)
+        {
+            var result = DBConnection.EditData(string.Format("UPDATE RepairOrders SET ='{0}', DateOfReceipt='{1}', Description ='{2}', Price ='{3}'", repairOrder.DateOfReceipt, repairOrder.Description, repairOrder.Price));
+
+            return result;
+        }
+
+        public int DeleteRepairOrder(RepairOrder repairOrder)
+        {
+            var result = DBConnection.EditData("DELETE FROM RepairOrders WHERE Id=" + repairOrder.Id);
+
+            return result;
+        }
+
+
     }
 }
